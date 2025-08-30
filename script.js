49c1851152383193afe34c1122253cffa62b84f9
eng-raghadsami <raghadsami.w@gmail.com>
2025-08-30 17:17:16 +0300

    Import quiz modules and initialize DOM elements

---
+++ script.js
@@ -0,0 +1,11 @@
+import { MultipleChoiceQuestion } from './modules/MultipleChoiceQuestion.js';
+import { TrueOrFalseQuestion } from './modules/TrueOrFalseQuestion.js';
+import { Question } from './modules/Question.js';
+import { Quiz } from './modules/Quiz.js';
+import { Storage } from "./modules/Storage.js";
+
+const quizContainer = document.getElementById("quiz-container");
+const quizTitle = document.getElementById("quiz-title");
+const quizForm = document.getElementById("quizForm");
+const result = document.getElementById("result");
+
